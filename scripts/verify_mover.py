8aedb2050498b23ea619541f115a7ccb88ccaef4
timothyas <smith.timothy.andrew@gmail.com>
2023-10-20 16:48:15 +0000

    simple verification done, append with zarr doesn't work as expected

--- scripts/verify_mover.py
+++ scripts/verify_mover.py
@@ -1,6 +1,8 @@
+
 from UFS2ARCO import FV3Dataset
 from replay_mover import ReplayMover1Degree
 from timer import Timer
+
 
 if __name__ == "__main__":
 
@@ -20,18 +22,3 @@ if __name__ == "__main__":
     job_id = 1000
     mover.run(job_id=job_id)
     localtime.stop()
-
-    # Now do this the ol fashioned way (by ol' fashioned I mean a week ago)
-    replay = FV3Dataset(path_in=mover.cached_path, config_filename="config-replay-local.yaml")
-
-    kw = {"mode": "w"}
-    for cycle in mover.my_cycles(job_id):
-
-        date = mover.npdate2datetime(cycle)
-        localtime.start(f"Reading {str(date)}")
-        xds = replay.open_dataset(date, **mover.ods_kwargs)
-        replay.store_dataset(xds, **kw)
-        kw = {"mode": "a", "append_dim": "time"}
-        localtime.stop()
-
-    walltime.stop("Total Walltime")
