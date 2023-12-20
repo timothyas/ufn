f3d372e63b49068d01a71b7d64c4d45fe05f853c
timothyas <smith.timothy.andrew@gmail.com>
2023-12-20 21:16:23 +0000

    this computes loss and gradient

--- replay-training/run_training.py
+++ replay-training/run_training.py
@@ -8,57 +8,7 @@ import optax
 from ufs2arco.timer import Timer
 
 from simple_emulator import ReplayEmulator1Degree
-from train import optim_step, run_forward, grads_fn
-
-
-
-def optimize(params, state, optimizer, emulator, input_batches, target_batches, forcing_batches):
-
-    opt_state = optimizer.init(params)
-
-    def with_em(fn):
-        return partial(fn, emulator=emulator)
-
-    def with_params(fn):
-        return partial(fn, params=params, state=state)
-
-    # Our models aren't stateful, so the state is always empty, so just return the
-    # predictions. This is required by our rollout code, and generally simpler.
-    #def drop_state(fn):
-    #    return lambda **kw: fn(**kw)[0]
-    optim_step_jitted = with_params( jit( #with_em(
-        optim_step
-    ) ) #)
-
-    print(" ... done jitting optim_step")
-
-    grads_jitted = with_params( jit(
-        grads_fn
-    ) )
-
-    print(" ... done jitting grads_fn")
-
-    loss, diagnostics, next_state, grads = grads_jitted(
-        emulator=emulator,
-        inputs=input_batches.sel(batch=[0]),
-        targets=target_batches.sel(batch=[0]),
-        forcings=forcing_batches.sel(batch=[0]),
-    )
-    return loss, grads
-
-
-
-    #for i in input_batches["batch"].values:
-
-    #    params, loss, diagnostics, opt_state, grads = optim_step_jitted(
-    #        emulator=emulator,
-    #        inputs=input_batches.sel(batch=[i]),
-    #        targets=target_batches.sel(batch=[i]),
-    #        forcings=forcing_batches.sel(batch=[i]),
-    #    )
-    #    print(f"Step = {i}, loss = {loss}")
-
-    #return params, loss, diagnostics, opt_state, grads
+from train import optimize, run_forward
 
 
 if __name__ == "__main__":
@@ -95,14 +45,6 @@ if __name__ == "__main__":
 
     localtime.start("Initializing Optimizer and Parameters")
 
-#   We may not need this with the pytree stuff defined
-#    # A note from the GraphCast demo:
-#    # Jax doesn't seem to like passing configs as args through the jit. Passing it
-#    # in via partial (instead of capture by closure) forces jax to invalidate the
-#    # jit cache if you change configs.
-#    def with_emulator(fn):
-#        return partial(fn, emulator=gufs)
-
     init_jitted = jit( run_forward.init )
     params, state = init_jitted(
         rng=PRNGKey(gufs.init_rng_seed),
@@ -116,7 +58,6 @@ if __name__ == "__main__":
 
     localtime.start("Starting Optimization")
 
-    #params, loss, diagnostics, opt_state, grads = optimize(
     loss, grads = optimize(
         params=params,
         state=state,
@@ -126,6 +67,7 @@ if __name__ == "__main__":
         target_batches=targets,
         forcing_batches=forcings,
     )
+    print("loss: ", loss)
 
     localtime.stop()
 
--- replay-training/train.py
+++ replay-training/train.py
@@ -5,6 +5,17 @@ resolution, variables, etc with these routines
 
 These functions are taken from the GraphCast demo:
     https://github.com/google-deepmind/graphcast/blob/main/graphcast_demo.ipynb
+
+Note that the functions:
+    - run_forward.init
+    - run_forward.apply
+    - loss_fn.apply
+    - grads_fn
+are jitted with wrappers that give and takeaway things like the task and model configs, and the state and params
+It looks like we don't have to do this with the emulator object being passed because
+we are registering it as a pytree.
+See the last few methods and lines of simple_emulatory.py, following this guidance:
+    https://jax.readthedocs.io/en/latest/faq.html#strategy-3-making-customclass-a-pytree
 """
 
 from functools import partial
@@ -18,6 +29,7 @@ from graphcast.casting import Bfloat16Cast
 from graphcast.autoregressive import Predictor
 from graphcast.xarray_tree import map_structure
 from graphcast.normalization import InputsAndResiduals
+from graphcast.xarray_jax import unwrap_data
 
 
 def construct_wrapped_graphcast(emulator):
@@ -56,8 +68,9 @@ def loss_fn(emulator, inputs, targets, forcings):
     predictor = construct_wrapped_graphcast(emulator)
     loss, diagnostics = predictor.loss(inputs, targets, forcings)
     return map_structure(
-        lambda x: xarray_jax.unwrap_data(x.mean(), require_jax=True),
+        lambda x: unwrap_data(x.mean(), require_jax=True),
         (loss, diagnostics))
+
 
 def grads_fn(params, state, emulator, inputs, targets, forcings):
     def _aux(params, state, i, t, f):
@@ -74,6 +87,7 @@ def grads_fn(params, state, emulator, inputs, targets, forcings):
         forcings,
     )
     return loss, diagnostics, next_state, grads
+
 
 def optim_step(params, state, emulator, inputs, targets, forcings):
     def _aux(params, state, i, t, f):
@@ -94,37 +108,43 @@ def optim_step(params, state, emulator, inputs, targets, forcings):
     return params, loss, diagnostics, state, grads
 
 
+def optimize(params, state, optimizer, emulator, input_batches, target_batches, forcing_batches):
 
-## Jax doesn't seem to like passing configs as args through the jit. Passing it
-## in via partial (instead of capture by closure) forces jax to invalidate the
-## jit cache if you change configs.
-#def with_emulator(fn):
-#    return partial(fn, emulator=emulator)
-#
-#
-## Always pass params and state, so the usage below is simpler
-#def with_params(fn):
-#    return partial(fn, params=params, state=state)
-#
-## Our models aren't stateful, so the state is always empty, so just return the
-## predictions. This is required by our rollout code, and generally simpler.
-#def drop_state(fn):
-#    return lambda **kw: fn(**kw)[0]
-#
-#
-#
-#def optimize(params, optimizer, emulator, input_batches, target_batches, forcing_batches):
-#
-#    opt_state = optimizer.init(params)
-#
-#    optim_step_jitted = drop_state( with_emulator( jit( with_emulator(
-#        optim_step
-#    ) ) ) )
-#
-#    for i, (inputs, targets, forcings) in enumerate(input_batches, target_batches, forcing_batches):
-#
-#        params, loss, diagnostics, opt_state, grads = optim_step_jitted(
-#            params, opt_state, emulator, inputs, targets, forcings)
-#        print(f"Step = {i}, loss = {loss}")
-#
-#    return params, loss, diagnostics, opt_state, grads
+    opt_state = optimizer.init(params)
+
+    def with_params(fn):
+        return partial(fn, params=params, state=state)
+
+    optim_step_jitted = with_params( jit(
+        optim_step
+    ) )
+
+    print(" ... done jitting optim_step")
+
+    grads_jitted = with_params( jit(
+        grads_fn
+    ) )
+
+    print(" ... done jitting grads_fn")
+
+    loss, diagnostics, next_state, grads = grads_jitted(
+        emulator=emulator,
+        inputs=input_batches.sel(batch=[0]),
+        targets=target_batches.sel(batch=[0]),
+        forcings=forcing_batches.sel(batch=[0]),
+    )
+    return loss, grads
+
+
+
+    #for i in input_batches["batch"].values:
+
+    #    params, loss, diagnostics, opt_state, grads = optim_step_jitted(
+    #        emulator=emulator,
+    #        inputs=input_batches.sel(batch=[i]),
+    #        targets=target_batches.sel(batch=[i]),
+    #        forcings=forcing_batches.sel(batch=[i]),
+    #    )
+    #    print(f"Step = {i}, loss = {loss}")
+
+    #return params, loss, diagnostics, opt_state, grads
